5989d0e1668ce653afcffd12e94d41b1b5700695
EaswarUS <80040514+EaswarUS@users.noreply.github.com>
2021-03-31 12:52:30 +0530

    Add files via upload

---
+++ system/Desktop.java
@@ -0,0 +1,18 @@
+package org.system;
+
+public class Desktop extends Computer {
+
+	public void desktopSize() {
+		System.out.println("28 inches");
+		
+
+	}
+	public static void main(String[] args) {
+		Desktop method = new Desktop();
+		method.computerModel();
+		method.desktopSize();
+		
+		
+	}
+
+}
