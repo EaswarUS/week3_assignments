c2b910b66557bfb9fdab99ee30aa79f8f4ae5a0c
EaswarUS <80040514+EaswarUS@users.noreply.github.com>
2021-03-31 14:27:33 +0530

    Add files via upload

---
+++ college/College.java
@@ -0,0 +1,19 @@
+package org.college;
+
+public class College {
+	
+	public void collegeName() {
+		System.out.println("Stella Mary's College Of Engineering");
+		
+	}
+	public void collegeCode() {
+		System.out.println("9623");
+		
+	}
+	public void collegeRank() {
+		System.out.println("10");
+		
+	}
+	
+
+}
